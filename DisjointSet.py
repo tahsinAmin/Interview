87fb1bf6cb47e492b215d66706e7b3921d3d9b94
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-07-21 10:20:04 +0600

    Disjoint Set in Python but without using class

---
+++ DisjointSet.py
@@ -0,0 +1,56 @@
+def find_set_representative(parentGraph, node):
+	if(parentGraph[node] < 0):
+		return node		
+	else:
+		rootNode = find_set_representative(parentGraph, parentGraph[node])
+		parentGraph[node] = rootNode
+		return rootNode
+
+def in_same_set(parentGraph, m, n):
+	if m is n:
+		return True
+	elif parentGraph[m] is -1:
+		return False
+	return in_same_set(parentGraph, parentGraph[m], n)
+
+def make_union(parentGraph, m, n):
+	mRoot =  find_set_representative(parentGraph, m)
+	nRoot =  find_set_representative(parentGraph, n)
+
+	if mRoot == nRoot:
+		return False
+	else:
+		if parentGraph[mRoot] == parentGraph[nRoot]:
+			parentGraph[mRoot] = parentGraph[mRoot] + parentGraph[nRoot]
+			parentGraph[nRoot] = mRoot
+		else:
+			if parentGraph[mRoot] < parentGraph[nRoot]:
+				parentGraph[mRoot] = parentGraph[nRoot] + parentGraph[mRoot]
+				parentGraph[nRoot] = mRoot
+			else:
+				parentGraph[nRoot] = parentGraph[nRoot] + parentGraph[mRoot]
+				parentGraph[mRoot] = nRoot
+
+n = 7
+parentGraph = {}
+
+for i in range(n):
+	parentGraph[i] = -1
+	
+ipt = [[0,1], [1,2], [2,3], [4,5]]
+print(parentGraph)
+
+for m,n in ipt:
+	make_union(parentGraph, m,n)
+	print(parentGraph)
+	
+print(parentGraph)
+
+print(find_set_representative(parentGraph,1))
+print(find_set_representative(parentGraph,0))
+make_union(parentGraph, 4,2)
+
+print(parentGraph)
+
+# https://www.youtube.com/watch?v=WQbbFhPGBN0
+# 23:44
